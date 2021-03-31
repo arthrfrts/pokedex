9e9b882335fb8935bf4c8875ef5287a9416f0241
Arthur Freitas <arthur.freitas@compasso.com.br>
2021-03-30 21:30:56 -0300

    feat(pagination): starts pagination handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,42 @@
 <script>
-  import { onMount } from "svelte";
   import ky from "ky";
   import PokeCard from "./components/PokeCard.svelte";
 
   let pokemons = [];
+  let offset = 0;
+  let perPage = 24;
 
-  onMount(async () => {
-    let api_url = "//pokeapi.co/api/v2/pokemon";
-    const data = await ky.get(api_url).json();
+  $: {
+    getPokemon(offset);
+  }
 
-    pokemons = data.results;
-  });
+  // Fetching Pokémons from the API.
+  const getPokemon = async () => {
+    let requestURL = `//pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${perPage}`;
+
+    const data = await ky.get(requestURL).json();
+
+    pokemons = [...pokemons, ...data.results];
+  };
+
+  // Setting the next batch to load.
+  const handlePagination = (event) => {
+    offset += perPage;
+  };
 </script>
 
 <div class="container">
   <ul class="pokemons">
-    {#each pokemons as { name, url, image }, index (index)}
+    {#each pokemons as { name, url } (url)}
       <li>
         <PokeCard {name} {url} />
       </li>
     {/each}
+
+    {#if pokemons.length > 0}
+      <button type="button" id="load-more" on:click={handlePagination}>
+        Load more Pokemons!
+      </button>
+    {/if}
   </ul>
 </div>
