957b2ea5c1470872e48ed3e640526ac2d69f39ec
Arthur Freitas <email@arthr.me>
2021-04-03 19:14:54 +0000

    feat(detail): main structure from pokemon info

--- src/components/PokeDetail.svelte
+++ src/components/PokeDetail.svelte
@@ -3,6 +3,7 @@
 
   import DataStats from "./DataStats.svelte";
   import Sprites from "./Sprite.svelte";
+  import Type from "./Type.svelte";
 
   export let pokemon;
 
@@ -10,99 +11,111 @@
   const maleColor = "--color-scale-blue-3";
 </script>
 
-<header>
-  <h2>{pokemon.name}</h2>
-  <p>{pokemon.description}</p>
+<style>
+  .pokedetails-header {
+    margin-bottom: 3rem;
+  }
 
-  <ul>
+  .pokedetails-header .pokemon-type-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+  }
+
+  .datagroups {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: var(--gutter);
+  }
+
+  @media screen and (max-width: 600px) {
+    .datagroups {
+      grid-template-columns: 1fr;
+    }
+  }
+</style>
+
+<header class="pokedetails-header">
+  <h2>{pokemon.name}</h2>
+
+  <ul class="pokemon-type-list">
     {#each pokemon.types as type (type.name)}
-      <li style="border-color: var({type.color}); color: var({type.color});">
-        {type.name}
+      <li>
+        <Type name={type.name} color={type.color} />
       </li>
     {/each}
   </ul>
+
+  <p>{pokemon.description}</p>
 </header>
 
-<figure>
-  <Sprites sprites={pokemon.sprites} />
-</figure>
+<Sprites sprites={pokemon.sprites} />
 
-<section>
-  <h3>Profile</h3>
-  <dl>
-    <dt>Species:</dt>
-    <dd>{pokemon.species}</dd>
-    <dt>Height:</dt>
-    <dd>
-      {pokemon.height / 10} m ({Math.round(
-        (pokemon.height * 0.328084 + 0.00001) * 100
-      ) / 100}
-      lb)
-    </dd>
-    <dt>Weight:</dt>
-    <dd>
-      {pokemon.weight / 10} kg ({Math.round(
-        (pokemon.weight * 0.220462 + 0.00001) * 100
-      ) / 100}
-      lb)
-    </dd>
-    <dt>Abilities:</dt>
-    <dd>{pokemon.abilities}</dd>
-  </dl>
-</section>
+<div class="datagroups">
+  <section class="dataset">
+    <h3>Profile</h3>
+    <dl>
+      <dt>Species:</dt>
+      <dd>{pokemon.species}</dd>
+      <dt>Height:</dt>
+      <dd>
+        {pokemon.height / 10} m ({Math.round(
+          (pokemon.height * 0.328084 + 0.00001) * 100
+        ) / 100}
+        lb)
+      </dd>
+      <dt>Weight:</dt>
+      <dd>
+        {pokemon.weight / 10} kg ({Math.round(
+          (pokemon.weight * 0.220462 + 0.00001) * 100
+        ) / 100}
+        lb)
+      </dd>
+      <dt>Abilities:</dt>
+      <dd>{pokemon.abilities}</dd>
+    </dl>
+  </section>
 
-<section>
-  <h3>Training Stats</h3>
-  <dl>
-    <dt>EV Yield:</dt>
-    <dd>{pokemon.evs}</dd>
-    <dt>Capture Rate:</dt>
-    <dd>{pokemon.captureRate}</dd>
-    <dt>Growth Rate:</dt>
-    <dd>{pokemon.growthRate}</dd>
-  </dl>
-</section>
+  <section class="dataset">
+    <h3>Training Stats</h3>
+    <dl>
+      <dt>EV Yield:</dt>
+      <dd>{pokemon.evs}</dd>
+      <dt>Capture Rate:</dt>
+      <dd>{pokemon.captureRate}</dd>
+      <dt>Growth Rate:</dt>
+      <dd>{pokemon.growthRate}</dd>
+    </dl>
+  </section>
 
-<section>
-  <h3>Breeding Stats</h3>
-  <dl>
-    <dt>Gender Ratio (%F / %M):</dt>
-    <dd>
-      <DataStats value={pokemon.genderRatio.female} statColor={femaleColor} />
-      <DataStats value={pokemon.genderRatio.male} statColor={maleColor} />
-    </dd>
-    <dt>Egg Groups:</dt>
-    <dd>
-      {#each pokemon.eggGroups as egg}{egg}{/each}
-    </dd>
-  </dl>
-</section>
+  <section class="dataset">
+    <h3>Breeding Stats</h3>
+    <dl>
+      <dt>Gender Ratio (%F / %M):</dt>
+      <dd>
+        <DataStats value={pokemon.genderRatio.female} statColor={femaleColor} />
+        <DataStats value={pokemon.genderRatio.male} statColor={maleColor} />
+      </dd>
+      <dt>Egg Groups:</dt>
+      <dd>
+        {#each pokemon.eggGroups as egg}{egg}{/each}
+      </dd>
+    </dl>
+  </section>
 
-<section>
-  <h2>Base Stats</h2>
-  <dl>
-    {#each pokemon.stats as stat}
-      <div>
+  <section class="dataset">
+    <h3>Base Stats</h3>
+    <dl>
+      {#each pokemon.stats as stat}
         <dt>{stat.name}</dt>
         <dd>
           <DataStats value={stat.base_stat} statColor={pokemon.detailTheme} />
         </dd>
-      </div>
-    {/each}
-  </dl>
-</section>
-
-<section>
-  <h3>Base Stats</h3>
-  <dl>
-    {#each pokemon.stats as stat}
-      <dt>{stat.name}</dt>
-      <dd>
-        <DataStats value={stat.base_stat} statColor={pokemon.detailTheme} />
-      </dd>
-    {/each}
-  </dl>
-</section>
+      {/each}
+    </dl>
+  </section>
+</div>
 
 <footer>
   {#if pokemon.evolvesFrom}
@@ -110,9 +123,9 @@
       {pokemon.name} evolves from
       <Link to="/{pokemon.evolvesFrom.url}">
         {pokemon.evolvesFrom.name}
-      </Link>
+      </Link>.
     </p>
   {:else}
-    <p>{pokemon.name} does not evolve from another Pokemon</p>
+    <p>{pokemon.name} does not evolve from another Pokemon.</p>
   {/if}
 </footer>
