ff807ea96ae0fb48d0bfa05362a63f5c0d8ecb16
Arthur Freitas <email@arthr.me>
2021-04-03 11:05:40 -0300

    feat(detail): fetch pokémons from API.

--- routes/Detail.svelte
+++ routes/Detail.svelte
@@ -1,6 +1,21 @@
 <script>
+  import ky from "ky";
+
   export let id;
-  console.debug(id);
+  export let pokemon;
+
+  const getPokemon = async (id) => {
+    const pokeURL = `https://pokeapi.co/api/v2/pokemon/${id}/`;
+    const pokeSpeciesURL = `https://pokeapi.co/api/v2/pokemon-species/${id}/`;
+
+    const pokeStats = await ky.get(pokeURL).json();
+    const pokeSpecies = await ky.get(pokeSpeciesURL).json();
+
+    console.debug(pokeStats);
+    console.debug(pokeSpecies);
+  };
+
+  $: getPokemon(id);
 </script>
 
 <h1>Detail Page</h1>
