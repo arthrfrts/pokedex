980397fca14577c96817ce1271be87a6c6d905fb
Arthur Freitas <email@arthr.me>
2021-04-03 18:35:54 +0000

    feat(list): styles the main list

--- src/components/PokeCard.svelte
+++ src/components/PokeCard.svelte
@@ -12,16 +12,29 @@
   const imgURL = `//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`;
 </script>
 
-<article>
-  <header>
-    <h2>
-      <Link to="/{index}">
-        {name.toUpperCase()}
-      </Link>
-    </h2>
-  </header>
-
+<figure class="pokecard">
   <Link to="/{index}">
-    <img alt="" src={imgURL} role="presentation" />
+    <img class="sprite" alt="{name.toUpperCase()} sprite" src={imgURL} role="presentation" />
   </Link>
-</article>
+
+  <figcaption>
+    <Link to="/{index}">
+      <span class="dex-id">#{index}</span>
+      <strong class="pokemon-name">{name.toUpperCase()}</strong>
+    </Link>
+  </figcaption>
+</figure>
+
+<style>
+  .pokecard {
+    margin: 0;
+    text-align: center;
+  }
+
+  .pokecard .sprite {
+    display: block;
+    max-width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
